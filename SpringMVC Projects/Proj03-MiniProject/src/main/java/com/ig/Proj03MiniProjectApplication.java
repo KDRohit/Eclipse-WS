e97903693df4393ec49020faa88b959362d5e4f8
INGENUITY\rdubey <rohitdubey0786@gmail.com>
2025-11-17 19:33:37 +0530

    mini project added

---
+++ SpringMVC Projects/Proj03-MiniProject/src/main/java/com/ig/Proj03MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj03MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj03MiniProjectApplication.class, args);
+	}
+
+}
---
+++ SpringMVC Projects/Proj03-MiniProject/src/main/java/com/ig/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ig;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Proj03MiniProjectApplication.class);
+	}
+
+}
