215c6a60251c362a97a0518043ce9081062d2b16
INGENUITY\rdubey <rohitdubey0786@gmail.com>
2026-06-07 17:10:50 +0530

    code updated

--- Docker Projects/Proj02-AdvanceApp/src/main/java/com/ig/controller/MovieController.java
+++ Docker Projects/Proj02-AdvanceApp/src/main/java/com/ig/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.ig.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,18 +19,21 @@ import com.ig.service.MovieService;
 @RequestMapping("/movie")
 public class MovieController 
 {
+	private static final Logger log = LoggerFactory.getLogger(MovieController.class);
 	@Autowired
 	public MovieService movieService;
 	
 	@GetMapping("/getall")
 	public Response<List<EntityMovie>> getAllMovies()
 	{
+		log.info("Tracing :: movie/getall");
 		return movieService.getAllMovies();
 	}
 	
 	@GetMapping("/save/{name}/{actor}/{actress}")
 	public Response<EntityMovie> saveMovie(@PathVariable String name, @PathVariable String actor, @PathVariable String actress)
 	{
+		log.info("Tracing :: movie/save");
 		EntityMovie movie = new EntityMovie(name, actor, actress);
 		return movieService.saveMovie(movie);
 	}
